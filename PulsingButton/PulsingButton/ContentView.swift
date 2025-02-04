c11c7171b45c8419b09e298566be6bfc392a0f55
AdrianSA <50012235+cloner69@users.noreply.github.com>
2025-02-04 11:01:30 +0700

    pulsing buttton

---
+++ PulsingButton/PulsingButton/ContentView.swift
@@ -0,0 +1,41 @@
+//
+//  ContentView.swift
+//  PulsingButton
+//
+//  Created by Adrian Suryo Abiyoga on 23/01/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var animationAmount = 1.0
+    
+    var body: some View {
+        Button("Tap Me") {
+            // animationAmount += 1
+            print("Tapped")
+        }
+        .padding(50)
+        .background(.red)
+        .foregroundStyle(.white)
+        .clipShape(.circle)
+        .overlay(
+            Circle()
+                .stroke(.red)
+                .scaleEffect(animationAmount)
+                .opacity(2 - animationAmount)
+                .animation(
+                    .easeInOut(duration: 1)
+                        .repeatForever(autoreverses: false),
+                    value: animationAmount
+                )
+        )
+        .onAppear {
+            animationAmount = 2
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PulsingButton/PulsingButton/PulsingButtonApp.swift
@@ -0,0 +1,17 @@
+//
+//  PulsingButtonApp.swift
+//  PulsingButton
+//
+//  Created by Adrian Suryo Abiyoga on 23/01/25.
+//
+
+import SwiftUI
+
+@main
+struct PulsingButtonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
